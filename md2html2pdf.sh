04b134bd6448ce5a100695b675c77f31c6a8ae6d
Purnanand Elango <purnanandelango@gmail.com>
2023-05-09 01:01:23 -0700

    First commit

---
+++ md2html2pdf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 input_file output_file"
+    exit 1
+fi
+
+input_file=$1
+output_file="$2"
+output_file="${output_file%.*}.pdf"  # Ensure output_file has a .pdf extension
+
+# Convert Markdown to HTML
+pandoc "$input_file" \
+  --standalone \
+  --metadata=title-from=1 \
+  --css=https://purnaelango.com/typeset-util/preview.css \
+  --variable=geometry:"top=0pt, bottom=0pt, left=0pt, right=0pt" \
+  -o temp.html
+
+# Convert HTML to PDF with wkhtmltopdf
+wkhtmltopdf --margin-top 0in --margin-right 0in --margin-bottom 0in --margin-left 0in temp.html "$output_file"
+
+# Remove temporary HTML file
+rm temp.html
---
+++ md2pdf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check that two arguments were provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <input-file> <output-file>"
+    exit 1
+fi
+
+# Set the input and output file names
+input_file="$1"
+output_file="$2"
+output_file="${output_file%.*}.pdf"  # Ensure output_file has a .pdf extension
+
+# Run Pandoc to convert the Markdown file to PDF
+pandoc "$input_file" \
+  --pdf-engine=pdflatex \
+  -V mainfont="Palatino" \
+  -V sansfont="Helvetica" \
+  -V monofont="Courier" \
+  -V mathfont="mathpazo" \
+  -V colorlinks=true \
+  -V urlcolor=blue \
+  -V version=2.0 \
+  -V fontsize=12pt \
+  -V geometry:margin=0.75in \
+  -o "$output_file"
